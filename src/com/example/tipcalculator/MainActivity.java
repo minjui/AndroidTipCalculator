b121f2d6d6039f1fcbba63100f23e5f01d091b29
Minjui Huang <minjui@yahoo.com>
2013-10-05 17:29:04 -0700

    use ButtonOnClick helper for all button activities

--- src/com/example/tipcalculator/MainActivity.java
+++ src/com/example/tipcalculator/MainActivity.java
@@ -6,50 +6,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	private EditText etMoney;
-	private TextView tvTipAmt;
-	private double money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        etMoney = (EditText) findViewById(R.id.etMoney);
-        
-        tvTipAmt = (TextView) findViewById(R.id.tvTipAmt);
-        
-        Button b10 = (Button) findViewById(R.id.btn10pct);
-        Button b15 = (Button) findViewById(R.id.btn15pct);
-        Button b20 = (Button) findViewById(R.id.btn20pct);
-        b10.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				double tipPct = 0.1;
-				showTip(tipPct);
-			}
-		});
-        b15.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				double tipPct = 0.15;
-				showTip(tipPct);
-			}
-		});
-        b20.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				double tipPct = 0.2;
-				showTip(tipPct);
-			}
-		});
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,10 +24,30 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    public void ButtonOnClick(View v) {
+    	double tipPct;
+    	switch (v.getId()) {
+    		case R.id.btn10pct:
+    			tipPct = 0.1;
+    			break;
+    		case R.id.btn15pct:
+    			tipPct = 0.15;
+    			break;
+    		case R.id.btn20pct:
+    			tipPct = 0.2;
+    			break;
+    		default:
+    			throw new RuntimeException("unknown button ID");
+    	}
+    	showTip(tipPct);
+    }
 
 	private void showTip(double tipPct) {
+        EditText etMoney = (EditText) findViewById(R.id.etMoney);
+        TextView tvTipAmt = (TextView) findViewById(R.id.tvTipAmt);
+
 		String pattern = "$###,###.##";
-		money = Double.parseDouble(etMoney.getText().toString());
+		double money = Double.parseDouble(etMoney.getText().toString());
 		Double a = money * tipPct;
 		DecimalFormat myFormatter = new DecimalFormat(pattern);
 		String output = myFormatter.format(a);
